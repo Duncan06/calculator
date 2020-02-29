d1329a9b46c8db9004e3986c49b81198058b7966
Rum <tylerdownie0@gmail.com>
2020-02-28 23:32:34 -0800

    can click for answers

--- calculator.js
+++ calculator.js
@@ -1,16 +1,9 @@
 
 
-let a = 0;
-let b = 0;
+let a = '';
+let b = '';
 let number = '';
 let option = '';
-// const output = document.getElementById('#number');
-
-// const selection = document.querySelectorAll('[data-number]');
-
-const operators = document.querySelectorAll('.operation');
-
-const solution = document.getElementById('#solve');
 
 function addition(a,b) {
     return a+b;
@@ -28,45 +21,60 @@ function division(a,b) {
     return a/b;
 }
 
-function operate(call,a,b) {
-    if (call = "addition") {
+function operates(call,a,b) {
+    if (call == "addition") {
         return addition(a,b);
     }
 
-    if (call = "subtraction") {
+    if (call == "subtraction") {
         return subtraction(a,b);
     }
 
-    if (call = "multiplication") {
+    if (call == "multiplication") {
         return multiplication(a,b);
     }
-    if (call = "division") {
+    if (call == "division") {
         return division(a,b);
     }
 }
 
-
- 
 function updateValue(){
-    console.log(this);
     number = number.concat(this.dataset.number)
     output.value = `${number}`;
-    console.log(number);
 }
 
-operators.forEach((operator) => {
-    operator.addEventListener('click', (e) => {
-        option = e.attr('id');
-        a = parseInt(number);
-        number = '';
-    })
-})
+function operate(){
+    option = this.id;
+    if (a == NaN) {
+        return;
+    }
+    a = parseFloat(number);
+    number = '';
+    return a;
+}
 
-solution.addEventListener('click', () => {
-    b = parseInt(number);
-    final = operate(option,a,b);
-    a = final;
-})
+function solve(){
+    b = parseFloat(number);
+    if (number == '' || a == '' || a == NaN) {
+        return;
+    }
+    final = operates(option,a,b);
+    number = `${final}`;
+    output.value = number;
+    b = '';
+    if (option == 'division'){
+        a = '';
+        return number, a, b;
+    }
+    return number, b;
+}
 
-// selection.forEach(num => num.addEventListener('click', updateValue));
+function clears(){
+    output.value = '';
+    number = '';
+    a = '';
+    b = '';
+    return number, a, b;
+}
+
 
